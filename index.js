a87f977a08e13fd365a73b074b99109ce7381c9a
ASHISH KUMAR <40332678+ashishnnnnn@users.noreply.github.com>
2021-09-15 21:15:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 var readlineSync = require('readline-sync');
 
-console.log("Let see how much you know about india!! ");
+var name = readlineSync.question("What Is Your Name? \n");
+
+console.log("Okay "+name+" Let see how much you know about india!! ");
 
 var question1 = {
-  question: "What is the capital of India? \n (a).Kolkata \n (b).Delhi \n (c).Patna \n (d). Arrah(Lolipop song waala)\n",
+  question: "(1). What is the capital of India? \n (a).Kolkata \n (b).Delhi \n (c).Patna \n (d). Arrah(Lolipop song waala)\n",
   answer: "b"
 };
 
 var question2 = {
-  question: "How many states are there in India? \n (a).28 \n (b).29 \n (c).27 \n (d).26\n",
+  question: "(2). How many states are there in India? \n (a).28 \n (b).29 \n (c).27 \n (d).26\n",
   answer: "b"
 };
 
 var question3 = {
-  question: "World rank of india in terms of Population? (a).1 \n (b).4 \n (c).2 \n (d).None of Above\n",
+  question: "(3). World rank of india in terms of Population? \n(a).1 \n (b).4 \n (c).2 \n (d).None of Above\n",
   answer: "c"
 };
 
 var question4 = {
-  question: "India's National Game? (a).Cricket\n (b).Kabaddi\n (c).Footaball\n (d).Hockey\n",
+  question: "(4). India's National Game? \n(a).Cricket\n (b).Kabaddi\n (c).Footaball\n (d).Hockey\n",
   answer: "d"
 }
 
 var question5 = {
-  question: "National fruit of India? (a).Mango\n (b).Banana\n (c).Apple\n (d).Orange\n",
+  question: "(5). National fruit of India? \n(a).Mango\n (b).Banana\n (c).Apple\n (d).Orange\n",
   answer: "a"
 };
 
-var questions = [question1,question2,question3,question4,question5];
+var question6 = {
+  question: "(6). What Is The Rank Of Indian Army In World In terms Of Strength? \n(a).4th\n (b).3rd\n (c).2nd\n (d).1st\n",
+  answer: "c"
+};
+
+var questions = [question1,question2,question3,question4,question5,question6];
 
 var score = 0;
 function play_game(query,answer){
@@ -35,9 +42,11 @@ function play_game(query,answer){
   if(user_ans===answer){
     console.log("You are Right");
     score+=1;
+    console.log("Your Score is",score);
   }
   else{
     console.log("You are Wrong");
+    console.log("Your Score is",score);
   }
 }
 
